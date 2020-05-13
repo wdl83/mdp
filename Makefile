141ece26d7cd81f0b0bd15bf97b2203b88e8b9ca
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2020-05-13 14:34:38 +0200

    Refactoring. Moved example echo worker to separate file.

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-all: Makefile.broker Makefile.client Makefile.worker
+all: Makefile.broker Makefile.client Makefile.echo_worker
 	make -f Makefile.broker
-	make -f Makefile.worker
+	make -f Makefile.echo_worker
 	make -f Makefile.client
 
 clean:
 	rm *.o -f
 	rm broker.elf -f
-	rm worker.elf -f
+	rm echo_worker.elf -f
 	rm client.elf -f
---
+++ echo_worker.cpp
@@ -0,0 +1,76 @@
+#include <unistd.h>
+
+#include "Worker.h"
+
+void help()
+{
+    std::cout
+        << "worker -a broker_address -s service_name"
+        << std::endl;
+}
+
+int main(int argc, char *const argv[])
+{
+    std::string address;
+    std::string serviceName;
+
+    for(char c; -1 != (c = ::getopt(argc, argv, "ha:s:"));)
+    {
+        switch(c)
+        {
+            case 'h':
+                help();
+                return EXIT_SUCCESS;
+                break;
+            case 'a':
+                address = optarg;
+                break;
+            case 's':
+                serviceName = optarg;
+                break;
+            case ':':
+            case '?':
+            default:
+                return EXIT_FAILURE;
+                break;
+        }
+    }
+
+    if(address.empty() || serviceName.empty())
+    {
+        help();
+        return EXIT_FAILURE;
+    }
+
+    try
+    {
+        Worker worker;
+
+        worker.exec(
+            address,
+            serviceName,
+            [](zmqpp::message message)
+            {
+                /* echo */
+                message.push_back("+worker");
+                return message;
+            });
+    }
+    catch(const EnsureException &except)
+    {
+        std::cerr << "ensure exception " << except.toString() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch(const std::exception &except)
+    {
+        std::cerr << "std exception " << except.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch(...)
+    {
+        std::cerr << "unsupported exception" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
