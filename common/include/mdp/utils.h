4ff9c91a1cf06728aa9b94cc40d156eccf7d43fd
Wlodzimierz Lipert <wlodzimierz.lipert@gmail.com>
2026-04-06 09:25:13 +0200

    Bugfix in lookup when converting to HEX.

--- common/include/mdp/utils.h
+++ common/include/mdp/utils.h
@@ -38,7 +38,7 @@ char toGlyph(int c)
     static const char lookup[] =
     {
         '0', '1', '2', '3', '4', '5', '6', '7',
-        '9', 'A', 'B', 'C', 'D', 'E', 'F'
+        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
     };
     return 16 > c ? lookup[c] : '?';
 }
